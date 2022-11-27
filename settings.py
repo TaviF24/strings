bd7881186601cb53727bf63c75d18426553392a0
TaviF24 <118764142+TaviF24@users.noreply.github.com>
2022-11-27 22:57:05 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,14 @@ if len(sys.argv)==3:
     setting = sys.argv[1]
     language = sys.argv[2]
     if setting=="1" and language=="RO":
-        f=open("settings.txt", "w")
+        f=open("settings.txt", "r")
+        sir= f.read()
+
+
+
+
+
+
         f.write(setting)
         f.write14("\n")
         f.write(language)
