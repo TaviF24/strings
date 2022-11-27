3031e048e269ac919b72aab453dab89fb87e24c0
TaviF24 <118764142+TaviF24@users.noreply.github.com>
2022-11-27 18:33:26 +0200

    settings

---
+++ settings.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.argv)==3:
+    setting = sys.argv[1]
+    language = sys.argv[2]
+    if setting=="1" and language=="RO":
+        f=open("settings.txt", "w")
+        f.write(setting)
+        f.write("\n")
+        f.write(language)
+        f.close()
+    elif setting=="0":
+        f = open("settings.txt", "w")
+        f.write(setting)
+        f.close()
